aae5c2fd64235328d0d510a0ca5cdd1e728cb6e6
Lean Rada <Kalabasa@users.noreply.github.com>
2021-12-12 13:54:32 +0800

    vertical

--- keyboards/lily58/keymaps/Kalabasa/main.c
+++ keyboards/lily58/keymaps/Kalabasa/main.c
@@ -3,15 +3,14 @@
 // TODO
 // * Swap Brightness keys
 // * Easier Ctrl+Tab
+// * Backspace in Layer 1
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
 
-// oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-//   if (is_keyboard_left())
-//     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-//   return rotation;
-// }
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  return OLED_ROTATION_270;
+}
 
 void oled_task_user(void) {
   oled_write_ln("Hello World!", false);
