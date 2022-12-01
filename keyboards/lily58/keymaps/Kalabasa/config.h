cccbd1f9454033f8abe9ebafad78680ffa2d8422
Kalabasa <Kalabasa@users.noreply.github.com>
2022-12-01 23:44:43 +1100

    Update

--- keyboards/lily58/keymaps/Kalabasa/config.h
+++ keyboards/lily58/keymaps/Kalabasa/config.h
@@ -23,13 +23,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_LEFT
 #define MASTER_RIGHT
 #define SPLIT_LAYER_STATE_ENABLE
-#define SPLIT_LED_STATE_ENABLE
+// #define SPLIT_LED_STATE_ENABLE
 #define SPLIT_MODS_ENABLE
 
 #define UNICODE_SELECTED_MODES UC_LNX
 
-#define TAPPING_TERM 175
-#define TAPPING_TOGGLE 2
+#define TAPPING_TERM 200
+// #define TAPPING_TOGGLE 2
 #define TAPPING_FORCE_HOLD_PER_KEY
 
 #define PERMISSIVE_HOLD
