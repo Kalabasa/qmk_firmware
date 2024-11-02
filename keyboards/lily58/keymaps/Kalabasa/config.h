5297f7207bbb3162bf46ed704eaabaadcfc3eeee
example@example.com <example@example.com>
2024-11-02 14:32:00 +1100

    Remove

--- keyboards/lily58/keymaps/Kalabasa/config.h
+++ keyboards/lily58/keymaps/Kalabasa/config.h
@@ -30,8 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
-// #define TAPPING_TOGGLE 2
-#define TAPPING_FORCE_HOLD_PER_KEY
 
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
